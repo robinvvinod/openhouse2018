48fb5d034988d08abda309e064c30c130f43a91d
Robin Vinod <robinvvinod@gmail.com>
2018-04-17 11:39:03 +0800

    Added support for iOS 10.0 and above

--- openhouse2018/AboutSSTViewController.swift
+++ openhouse2018/AboutSSTViewController.swift
@@ -10,21 +10,9 @@ import UIKit
 import WebKit
 
 class AboutSSTViewController: UIViewController, UIWebViewDelegate {
-
-    @IBOutlet weak var webview: WKWebView!
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
-        let url = URL(string: "http://www.sst.edu.sg/our-school/about-sst/")
-        let request = URLRequest(url: url!)
-        
-        webview.load(request)
-        
-        func webViewDidFinishLoad(webView: UIWebView!) {
-            print("Webview did finish load")
-        }
-        */
         
     }
 
