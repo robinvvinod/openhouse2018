8ca7771eb2382d79958323da197f65c6dad00bfd
Eugene Long <long_yee_jun_eugene@s2015.sst.edu.sg>
2018-04-23 16:14:30 +0800

    desperate times

--- openhouse2018/CurriculumViewController.swift
+++ openhouse2018/CurriculumViewController.swift
@@ -37,19 +37,22 @@ class CurriculumViewController: UIViewController, UICollectionViewDelegate, UICo
 		return cell
 	}
 	
-	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-		url = links[indexPath.item]
-		performSegue(withIdentifier: "showWebView", sender: self)
+	override func viewDidLayoutSubviews() {
+		self.curriculumCollectionView.collectionViewLayout.invalidateLayout()
 	}
-	
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
+		view.layoutIfNeeded()
 
         // Do any additional setup after loading the view.
 		curriculumCollectionView.delegate = self
 		curriculumCollectionView.dataSource = self
     }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		self.curriculumCollectionView.reloadData()
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -58,11 +61,9 @@ class CurriculumViewController: UIViewController, UICollectionViewDelegate, UICo
     
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-		if segue.identifier == "showWebView" {
-			let vc = segue.destination as! WebViewController
-			vc.url = url
-		}
+		
     }
+	
 	
 
 }
@@ -70,4 +71,78 @@ class CurriculumViewController: UIViewController, UICollectionViewDelegate, UICo
 class CurriculumCollectionViewCell: UICollectionViewCell {
 	@IBOutlet weak var curriculumFlavourImage: UIImageView!
 	@IBOutlet weak var curriculumText: UILabel!
+}
+
+class ShadowView: UIView {
+	//Source: https://github.com/benboecker/ShadowView
+	
+	/// The corner radius of the `ShadowView`, inspectable in Interface Builder
+	@IBInspectable var cornerRadius: CGFloat = 5.0 {
+		didSet {
+			self.updateProperties()
+		}
+	}
+	/// The shadow color of the `ShadowView`, inspectable in Interface Builder
+	@IBInspectable var shadowColor: UIColor = UIColor.black {
+		didSet {
+			self.updateProperties()
+		}
+	}
+	/// The shadow offset of the `ShadowView`, inspectable in Interface Builder
+	@IBInspectable var shadowOffset: CGSize = CGSize(width: 0.0, height: 2) {
+		didSet {
+			self.updateProperties()
+		}
+	}
+	/// The shadow radius of the `ShadowView`, inspectable in Interface Builder
+	@IBInspectable var shadowRadius: CGFloat = 4.0 {
+		didSet {
+			self.updateProperties()
+		}
+	}
+	/// The shadow opacity of the `ShadowView`, inspectable in Interface Builder
+	@IBInspectable var shadowOpacity: Float = 0.5 {
+		didSet {
+			self.updateProperties()
+		}
+	}
+	
+	/**
+	Masks the layer to it's bounds and updates the layer properties and shadow path.
+	*/
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		
+		self.layer.masksToBounds = false
+		
+		self.updateProperties()
+		self.updateShadowPath()
+	}
+	
+	/**
+	Updates all layer properties according to the public properties of the `ShadowView`.
+	*/
+	fileprivate func updateProperties() {
+		self.layer.cornerRadius = self.cornerRadius
+		self.layer.shadowColor = self.shadowColor.cgColor
+		self.layer.shadowOffset = self.shadowOffset
+		self.layer.shadowRadius = self.shadowRadius
+		self.layer.shadowOpacity = self.shadowOpacity
+	}
+	
+	/**
+	Updates the bezier path of the shadow to be the same as the layer's bounds, taking the layer's corner radius into account.
+	*/
+	fileprivate func updateShadowPath() {
+		self.layer.shadowPath = UIBezierPath(roundedRect: layer.bounds, cornerRadius: layer.cornerRadius).cgPath
+	}
+	
+	/**
+	Updates the shadow path everytime the views frame changes.
+	*/
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		self.updateShadowPath()
+	}
 }
