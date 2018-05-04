ba7478e8c133e4b3fbebcc119f59ae05a4e74742
Eugene Long <long_yee_jun_eugene@s2015.sst.edu.sg>
2018-05-05 00:35:27 +0800

    Actually finished Curriculum View
    Changed to tableview and has schedule attached to it

--- openhouse2018/Curriculum/CurriculumInfo.swift
+++ openhouse2018/Curriculum/CurriculumInfo.swift
@@ -8,63 +8,63 @@
 
 import UIKit
 
-struct currSubjects {
-	var subjects: [String]
-	var images: [UIImage]
-	var locations: [String]?
-	var timings: [[String]]?
+struct Subject {
+	var subject: String
+	var location: String
+	var timings: [String]?
 	
-	func nextTiming(forSubject subject: String) -> String? {
-		if !subjects.contains(subject) {
-			return nil
-		}
-		if timings == nil {
-			return nil
-		}
-		var timeStrings : [String]!
+	func nextTiming() -> [String]? {
 		
-		if (timings?.count)! > 1 {
-			timeStrings = timings![subjects.index(of: subject)!]
-		} else {
-			timeStrings = timings![0]
-		}
-		
-		var nextPossibleTime: String?
-		
-		for timeString in timeStrings {
-			let startingHour = Int(timeString.prefix(2))
-			if Calendar.current.component(.hour, from: Date()) < startingHour! {
-				nextPossibleTime = timeString
-				break
+		if timings != nil && Calendar.current.component(.month, from: Date()) == 5 && Calendar.current.component(.day, from: Date()) == 26 {
+			for timeString in timings! {
+				let startingHour = Int(timeString.prefix(2))
+				let startingMinute = Int(timeString.prefix(5).suffix(2))
+				
+				let beforeStartingHour = Calendar.current.component(.hour, from: Date()) < startingHour!
+				let beforeStartingMinute = (Calendar.current.component(.minute, from: Date()) < startingMinute! && Calendar.current.component(.hour, from: Date()) == startingHour!)
+				
+				if beforeStartingHour || beforeStartingMinute {
+					let minutesToStart = startingMinute! - Calendar.current.component(.minute, from: Date()) + ((startingHour! - Calendar.current.component(.hour, from: Date()))*60)
+					return [timeString, (String(minutesToStart/60) +  "h " + String(minutesToStart%60) + "min")]
+				}
 			}
 		}
-		return nextPossibleTime ?? nil
-	}
-	
-	func getImage(forSubject subject: String) -> UIImage {
-		if images.count > 1 {
-			return images[subjects.index(of: subject)!]
-		}
-		return images[0]
+		
+		//No timings available
+		return nil
 	}
 }
 
-var ShowcaseSubjects = currSubjects(subjects: [
-	"Chemistry",
-	"Physics",
-	"Biology",
-	"Biotechnology",
-	"Electronics"
-	], images: [
-	#imageLiteral(resourceName: "currIcon")
-	], locations: [
-	"Chem Lab 2 (Block C Lvl 1)",
-	"Bio Lab 2 (Block C Lvl 1)",
-	"Bio Lab 1 (Block C Lvl 1)",
-	"Biotech Lab 1 (Block C Lvl 1)",
-	"Engineering Lab (Block C Lvl 2)"
-	], timings: [
-	["10:30 - 11:30","12:30 - 13:30","14:00 - 15:00","15:30 - 16:30"]
-	])
+var showcases = [
+	Subject(subject: "Chemistry", location: "Chem Lab 2 (Block C Lvl 1)", timings: ["10:30 - 11:30", "12:30 - 13:30", "14:00 - 15:00", "15:30 - 16:30"]),
+	Subject(subject: "Physics", location: "Bio Lab 2 (Block C Lvl 1)", timings: ["10:30 - 11:30", "12:30 - 13:30", "14:00 - 15:00", "15:30 - 16:30"]),
+	Subject(subject: "Biology", location: "Bio Lab 1 (Block C Lvl 1)", timings: ["10:30 - 11:30", "12:30 - 13:30", "14:00 - 15:00", "15:30 - 16:30"]),
+	Subject(subject: "Biotechnology", location: "Biotech Lab 1 (Block C Lvl 1)", timings: ["10:30 - 11:30", "12:30 - 13:30", "14:00 - 15:00", "15:30 - 16:30"]),
+	Subject(subject: "Electronics", location: "Engineering Lab - FOE(Block C Lvl 2)", timings: ["10:30 - 11:30", "12:30 - 13:30", "14:00 - 15:00", "15:30 - 16:30"]),
+	Subject(subject: "Science TDP", location: "Physics Lab 1 (Block C Lvl 2)", timings: nil)
+]
 
-var CurriculumSubjectList = [ShowcaseSubjects]
+var ccas = [
+	Subject(subject: "Astronomy", location: "Blk A Atrium, Foyer", timings: nil),
+	Subject(subject: "Athletics (Track)", location: "Blk A Atrium, Foyer", timings: nil),
+	Subject(subject: "Basketball", location: "Blk A Atrium, Foyer", timings: nil),
+	Subject(subject: "Fencing", location: "Blk A Atrium, Foyer", timings: nil),
+	Subject(subject: "Media Club", location: "Blk A Atrium, Foyer", timings: nil),
+	Subject(subject: "Showchoir", location: "Blk A Atrium, Foyer", timings: nil),
+	Subject(subject: "Scouts", location: "Blk A Atrium, Foyer", timings: nil),
+	Subject(subject: "Robotics", location: "Blk A Atrium, Foyer", timings: nil),
+	Subject(subject: "SYFC", location: "Blk A Atrium, Foyer", timings: nil)
+]
+
+var academics = [
+	Subject(subject: "ACE", location: "Level 2, Outside Maker Lab", timings: nil),
+	Subject(subject: "English", location: "Level 2, Outside Maker Lab", timings: nil),
+	Subject(subject: "IH", location: "Level 2, Outside Maker Lab", timings: nil),
+	Subject(subject: "Mathematics", location: "Level 2, Outside Maker Lab", timings: nil),
+	Subject(subject: "Chinese", location: "Level 2, Outside Maker Lab", timings: nil),
+	Subject(subject: "Malay", location: "Level 2, Outside Maker Lab", timings: nil),
+	Subject(subject: "Tamil", location: "Level 2, Outside Maker Lab", timings: nil),
+	Subject(subject: "S & W", location: "Level 2, Outside Maker Lab", timings: nil)
+]
+
+var data = [showcases, ccas, academics]
